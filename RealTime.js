a9fbf1dd7d42b51521b4b7c132a353689c2c366e
Arni Reynir <arni.reynir89@gmail.com>
2013-05-06 15:00:48 +0000

    light weight

--- RealTime.js
+++ RealTime.js
@@ -1,13 +1,17 @@
 var app = require('express')()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server, {transports:['flashsocket', 'websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']});
 
 server.listen(3000);
 
 var bleh = [];
 
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 io.sockets.on('connection', function (socket) {
-  // socket.emit('news', { hello: 'world' });
   socket.on('UpdateHeader', function(message){
     socket.emit('news', message);
   });
